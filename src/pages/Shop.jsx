23c38f04a94f98c88e4a2ac673e68c4e49221873
Richard Lefevre <rlefevre.6505@gmail.com>
2026-03-16 12:56:28 +0000

    fix: changed image sources

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,16 +1,24 @@
 import { items } from "../../data/data.js";
 import ItemCard from "../components/ItemCard.jsx";
+import { useState } from "react";
 // import { Link } from "react-router";
 import "./Shop.css";
 
 export default function ShopPage() {
+  const [view, setView] = useState([]);
   // const navigate = useNavigate();
 
   return (
-    <div className="product-container">
-      {items.map((item, i) => {
-        return <ItemCard key={`item${i}`} item={item} i={i} />;
-      })}
-    </div>
+    <>
+      <div className="shop_filters">
+        <select></select>
+      </div>
+
+      <div className="product-container">
+        {items.map((item, i) => {
+          return <ItemCard key={`item${i}`} item={item} i={i} />;
+        })}
+      </div>
+    </>
   );
 }
