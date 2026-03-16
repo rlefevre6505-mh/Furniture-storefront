a8ce2aae6b2e65ba546ac2a963ad459c1e288ebf
Richard Lefevre <rlefevre.6505@gmail.com>
2026-03-16 13:17:30 +0000

    fix: re-optimised hero images

--- data/data.js
+++ data/data.js
@@ -53,7 +53,7 @@ export const items = [
       "A contemporary dining chair with a warm oatmeal coloured fabric upholstery and black metal legs, perfect for creating a cozy atmosphere in your dining room.",
     price: "54.99",
     image:
-      "/public/images/chairs/bari-oatmeal-mottled-velvet-dining-chairs-with-black-legs.webp",
+      "/images/chairs/bari-oatmeal-mottled-velvet-dining-chairs-with-black-legs.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -69,7 +69,7 @@ export const items = [
       "A sleek and stylish dining chair with a dark grey faux leather upholstery and black metal legs, perfect for adding a modern touch to your dining space.",
     price: "59.99",
     image:
-      "/public/images/chairs/billie-dark-grey-faux-leather-dining-chairs-with-black-metal-legs.webp",
+      "/images/chairs/billie-dark-grey-faux-leather-dining-chairs-with-black-metal-legs.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -84,7 +84,7 @@ export const items = [
     description:
       "A classic dining chair with a brown linen upholstery and wooden legs, perfect for a traditional dining room.",
     price: "49.99",
-    image: "/public/images/chairs/cabrini-dining-chairs-brown-linen.webp",
+    image: "/images/chairs/cabrini-dining-chairs-brown-linen.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -100,7 +100,7 @@ export const items = [
       "A modern dining chair with a grey chenille fabric and chrome metal base, perfect for a contemporary dining space.",
     price: "69.99",
     image:
-      "/public/images/chairs/capri-grey-chenille-dining-chairs-with-chrome-metal-base.webp",
+      "/images/chairs/capri-grey-chenille-dining-chairs-with-chrome-metal-base.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -116,7 +116,7 @@ export const items = [
       "A stylish accent chair with a bright yellow fabric upholstery and black metal legs, perfect for adding a pop of colour to your living room or bedroom.",
     price: "49.49",
     image:
-      "/public/images/chairs/clifton-accent-chair-yellow-fabric-accent-chair.webp",
+      "/images/chairs/clifton-accent-chair-yellow-fabric-accent-chair.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -132,7 +132,7 @@ export const items = [
       "A comfortable swivel armchair with a soft antique green mottled velvet fabric, perfect for relaxing in your living room.",
     price: "199.99",
     image:
-      "/public/images/chairs/colette-360-degree-swivel-armchair-antique-green-mottled-velvet.webp",
+      "/images/chairs/colette-360-degree-swivel-armchair-antique-green-mottled-velvet.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -148,7 +148,7 @@ export const items = [
       "A stylish upholstered bench with a soft grey fabric upholstery and wooden legs, perfect for adding a touch of elegance to your living room or bedroom.",
     price: "149.99",
     image:
-      "/public/images/chairs/isla-upholstered-bench-luxury-cream-textured-chenille.webp",
+      "/images/chairs/isla-upholstered-bench-luxury-cream-textured-chenille.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -164,7 +164,7 @@ export const items = [
       "A set of modern dining chairs with a sleek black faux leather upholstery and chrome metal legs, perfect for a contemporary dining space.",
     price: "129.99",
     image:
-      "/public/images/chairs/juno-black-faux-leather-dining-chairs-with-chrome-legs.webp",
+      "/images/chairs/juno-black-faux-leather-dining-chairs-with-chrome-legs.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -180,7 +180,7 @@ export const items = [
       "A set of modern dining chairs with comfortable fabric upholstery and oak legs, perfect for a contemporary dining space.",
     price: "129.99",
     image:
-      "/public/images/chairs/lusso-360-degree-swivel-dining-chairs-with-solid-oak-legs-toffee-textured-fabric.webp",
+      "/images/chairs/lusso-360-degree-swivel-dining-chairs-with-solid-oak-legs-toffee-textured-fabric.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -196,7 +196,7 @@ export const items = [
       "A set of modern dining chairs with a soft cream chenille fabric and black metal legs, perfect for a contemporary dining space.",
     price: "129.99",
     image:
-      "/public/images/chairs/nori-modern-dining-chairs-cream-chenille-with-black-metal-legs.webp",
+      "/images/chairs/nori-modern-dining-chairs-cream-chenille-with-black-metal-legs.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -212,7 +212,7 @@ export const items = [
       "A comfortable electric swivel recliner with zero gravity functionality and a soft pumpkin chenille fabric, perfect for relaxing in your living room.",
     price: "299.99",
     image:
-      "/public/images/chairs/utah-zero-gravity-swivel-electric-recliner-armchair-pumpkin-chenille-fabric.webp",
+      "/images/chairs/utah-zero-gravity-swivel-electric-recliner-armchair-pumpkin-chenille-fabric.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -227,7 +227,7 @@ export const items = [
     description:
       "A modern armchair with a sleek design and a soft stone linen fabric, perfect for adding a touch of elegance to your living room.",
     price: "199.99",
-    image: "/public/images/chairs/windsor-modern-armchair-stone-linen.webp",
+    image: "/images/chairs/windsor-modern-armchair-stone-linen.webp",
     dimensions: "",
     materials: "",
     finish: "",
@@ -243,7 +243,7 @@ export const items = [
       "A stylish swivel accent chair with a soft oatmeal textured weave fabric, perfect for adding a touch of elegance to your living room.",
     price: "149.99",
     image:
-      "/public/images/chairs/zuri-swivel-accent-chair-oatmeal-soft-textured-weave-fabric.webp",
+      "/images/chairs/zuri-swivel-accent-chair-oatmeal-soft-textured-weave-fabric.webp",
     dimensions: "",
     materials: "",
     finish: "",
