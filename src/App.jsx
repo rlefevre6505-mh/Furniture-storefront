c7165d68b32626af0124b5443f8631860da50f77
Richard Lefevre <rlefevre.6505@gmail.com>
2026-03-31 17:56:10 +0100

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ProductDetails from "./pages/Product";
 import AboutPage from "./pages/AboutPage";
 import Checkout from "./pages/Checkout";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import CartProvider from "./context/CartContext";
 import PaymentSuccess from "./pages/PaymentSuccess";
 import PaymentFailure from "./pages/PaymentFailure";
@@ -27,6 +28,7 @@ export default function App() {
           <Route path="/cancel" element={<PaymentFailure />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </CartProvider>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router";
+import "./Footer.css";
+import { XIcon, FacebookIcon, InstagramIcon } from "./icons.jsx";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div>
+        <div className="links">
+          {/* Brand */}
+          <div className="links_div">
+            <div className="brand_div">
+              <h3 className="h3">Stoneleaf Furnishings</h3>
+            </div>
+            <p className="p">
+              Premium furniture for your modern lifestyle. Quality
+              craftsmanship, timeless design.
+            </p>
+          </div>
+
+          {/* Customer Service */}
+          <div className="links_div">
+            <h3>Customer Service</h3>
+            <a href="#">Shipping Information</a>
+            <a href="#">Returns & Exchanges</a>
+            <a href="#">Warranty</a>
+            <a href="#">Assembly Services</a>
+          </div>
+
+          {/* Quick Links */}
+          <div className="links_div">
+            <h3>Quick Links</h3>
+            <Link to="/shop">Shop All</Link>
+            <Link to="/about">About Us</Link>
+            <a href="#">Contact</a>
+            <a href="#">FAQ</a>
+          </div>
+        </div>
+
+        {/* Bottom Bar */}
+        <div className="social_links">
+          <p>© 2026 Stoneleaf Furnishings. All rights reserved.</p>
+          <div className="icons">
+            <a className="icon" href="#">
+              <FacebookIcon />
+            </a>
+            <a className="icon" href="#">
+              <InstagramIcon />
+            </a>
+            <a className="icon" href="#">
+              <XIcon />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
